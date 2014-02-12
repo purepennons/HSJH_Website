3cfcc850a895a2d9ee7df2de3348a41fee8d7900
purepennons <y80811@gmail.com>
2014-02-13 01:43:52 +0800

    photo of impression can be selected.

--- js/action.js
+++ js/action.js
@@ -4,3 +4,24 @@ function indexCarouselRepeat(){  //首頁輪播設定
         pause:'false'
     })
 }
+
+function onClickMemberPhoto(aId, textId) {
+	for(i=1;i<=6;i++){
+		var aTagId = 'member' + i;    //初始化大頭貼與心得狀態
+		var sectionText = 'text-' + i;
+		document.getElementById(aTagId).style.opacity = 0.5;
+		document.getElementById(aTagId).style.filter = 'alpha(Opacity=50)';  //for ie 7
+		document.getElementById(sectionText).style.display = 'none';
+	}
+	document.getElementById(aId).style.opacity = 1.0;
+	document.getElementById(aId).style.filter = 'alpha(Opacity=100)';  //for ie 7
+	document.getElementById(textId).style.display = 'block';
+}
+
+function test (){
+	alert(123);
+}
+
+function onMouseOutMemberPhoto(textId) {
+	document.getElementById(textId).style.display = 'none';
+}
