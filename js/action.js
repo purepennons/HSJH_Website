eb3dea87dac681266c0b4a7d9bac73d962147418
purepennons <y80811@gmail.com>
2014-02-13 23:24:36 +0800

    fixed bugs of impression.htm.

--- js/action.js
+++ js/action.js
@@ -8,25 +8,35 @@ function indexCarouselRepeat(){  //首頁輪播設定
 function onClickMemberPhoto(numOfMember) {
 	var colors = ['#FF0000', '#FF7519', '#FFFF19', '#66FF66', '#6699FF', '#8533D6']; 
 	for(i=1;i<=6;i++){
-		var aTagId = 'member' + i;    //初始化大頭貼與心得狀態
+		var imgTagId = 'member' + i;    //初始化大頭貼與心得狀態
 		var sectionText = 'text-' + i;
-		document.getElementById(aTagId).style.opacity = 0.5;
-		document.getElementById(aTagId).style.filter = 'alpha(Opacity=50)';  //for ie 7
+		document.getElementById(imgTagId).style.opacity = 0.5;
+		document.getElementById(imgTagId).style.filter = 'alpha(Opacity=50)';  //for ie 7
 		document.getElementById(sectionText).style.display = 'none';
 	}
-	aTagId = 'member' + numOfMember;
+	imgTagId = 'member' + numOfMember;
 	sectionText = 'text-' + numOfMember;
-	document.getElementById(aTagId).style.opacity = 1.0;
-	document.getElementById(aTagId).style.filter = 'alpha(Opacity=100)';  //for ie 7
-	//alert(colors[numOfMember]);
+	document.getElementById(imgTagId).style.opacity = 1.0;
+	document.getElementById(imgTagId).style.filter = 'alpha(Opacity=100)';  //for ie 7
 	document.getElementById(sectionText).style.borderColor = colors[numOfMember-1];
 	document.getElementById(sectionText).style.display = 'block';
+	document.getElementById('p').title = numOfMember;
 }
 
-function test (){
-	alert(123);
+function onMouseOverMemberPhoto(numOfMember){
+	var imgTagId = 'member' + numOfMember;
+	var pValue = 'member' + document.getElementById('p').title;
+	document.getElementById(imgTagId).style.opacity = 1.0;
+	document.getElementById(imgTagId).style.filter = 'alpha(Opacity=100)';  //for ie 7
+	document.getElementById(pValue).style.opacity = 1.0;
+	document.getElementById(pValue).style.filter = 'alpha(Opacity=100)';  //for ie 7	
 }
 
-function onMouseOutMemberPhoto(textId) {
-	document.getElementById(textId).style.display = 'none';
+function onMouseOutMemberPhoto(numOfMember) {
+	var imgTagId = 'member' + numOfMember;
+	var pValue = 'member' + document.getElementById('p').title;
+	document.getElementById(imgTagId).style.opacity = 0.5;
+	document.getElementById(imgTagId).style.filter = 'alpha(Opacity=50)';  //for ie 7
+	document.getElementById(pValue).style.opacity = 1.0;
+	document.getElementById(pValue).style.filter = 'alpha(Opacity=100)';  //for ie 7	
 }
